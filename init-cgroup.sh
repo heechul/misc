59bceba096ad4d64d46bdbac2d172bede0be882d
Heechul Yun <heechul@illinois.edu>
2012-12-26 16:22:29 -0600

    scripts: ..

--- init-cgroup.sh
+++ init-cgroup.sh
@@ -23,6 +23,10 @@ set_core_cgroup()
     core=$1
     patt=$2
     mask=$3
+    for t in `cat /sys/fs/cgroup/core${core}/tasks`; do
+	echo $t > /sys/fs/cgroup/tasks
+    done
+    rmdir /sys/fs/cgroup/core${core}
     mkdir /sys/fs/cgroup/core${core}
     pushd /sys/fs/cgroup/core${core}
     echo $core > cpuset.cpus
@@ -38,13 +42,25 @@ TARGET=$1
 
 echo "target=$TARGET"
 if [ "$TARGET" = "odroid" ]; then
-	set_core_cgroup 0 0  12
-	set_core_cgroup 1 4  12
-	set_core_cgroup 2 8  12
-	set_core_cgroup 3 12 12
+    NCOLOR=16
+    set_core_cgroup 0 0  12
+    set_core_cgroup 1 4  12
+    set_core_cgroup 2 8  12
+    set_core_cgroup 3 12 12
 elif [ "$TARGET" = "core2quad" ]; then
-	set_core_cgroup 1 0 32
-	set_core_cgroup 1 32 32
-	set_core_cgroup 2 0 32
-	set_core_cgroup 3 32 32
+    NCOLOR=64
+    set_core_cgroup 0 0 32
+    set_core_cgroup 1 32 32
+    set_core_cgroup 2 0 32
+    set_core_cgroup 3 32 32
+elif [ "$TARGET" = "core2quadB" ]; then
+    NCOLOR=32
+    set_core_cgroup 0 0 16
+    set_core_cgroup 1 16 16
+    set_core_cgroup 2 0 16
+    set_core_cgroup 3 16 16
 fi
+
+echo $NCOLOR  > /sys/kernel/debug/color_page_alloc/colors
+echo 2 > /sys/kernel/debug/color_page_alloc/debug_level
+echo 2 > /sys/kernel/debug/color_page_alloc/enable
