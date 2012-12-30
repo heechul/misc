b62097915d90b1e748162e55606fae9fe7405b68
Heechul Yun <heechul@illinois.edu>
2012-12-30 17:24:31 -0600

    new phdusa interface

--- init-cgroup.sh
+++ init-cgroup.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-
 init_system()
 {
+    serivce lightdm stop
     if !(mount | grep cgroup); then
 	mount -t cgroup xxx /sys/fs/cgroup
     fi
@@ -22,8 +21,7 @@ set_system_cgroup()
 set_core_cgroup()
 {
     core=$1
-    patt=$2
-    mask=$3
+    colors=$2
     for t in `cat /sys/fs/cgroup/core${core}/tasks`; do
 	echo $t > /sys/fs/cgroup/tasks
     done
@@ -32,43 +30,17 @@ set_core_cgroup()
     pushd /sys/fs/cgroup/core${core}
     echo $core > cpuset.cpus
     echo 0 > cpuset.mems
-    echo $patt > phdusa.phys_pattern
-    echo $mask > phdusa.phys_mask
+    echo $colors > phdusa.colors
 }
 
 init_system
 
-TARGET=$1
-[ -z "$TARGET" ] && TARGET=odroid
+set_core_cgroup 0 "0-1"
+set_core_cgroup 1 "2-3"
+set_core_cgroup 2 "4-5"
+set_core_cgroup 3 "6-7"
 
-echo "target=$TARGET"
-if [ "$TARGET" = "odroid" ]; then
-    NCOLOR=16
-    set_core_cgroup 0 0  12
-    set_core_cgroup 1 4  12
-    set_core_cgroup 2 8  12
-    set_core_cgroup 3 12 12
-elif [ "$TARGET" = "core2quad" ]; then
-    NCOLOR=64
-    set_core_cgroup 0 0 32
-    set_core_cgroup 1 32 32
-    set_core_cgroup 2 0 32
-    set_core_cgroup 3 32 32
-elif [ "$TARGET" = "core2quadB" ]; then
-    NCOLOR=32
-    set_core_cgroup 0 0 16
-    set_core_cgroup 1 16 16
-    set_core_cgroup 2 0 16
-    set_core_cgroup 3 16 16
-elif [ "$TARGET" = "i5" ]; then
-    NCOLOR=64
-    set_core_cgroup 0 0 1
-    set_core_cgroup 1 0 1
-    set_core_cgroup 2 1 1
-    set_core_cgroup 3 1 1
-fi
-
-echo $NCOLOR  > /sys/kernel/debug/color_page_alloc/colors
-echo 2 > /sys/kernel/debug/color_page_alloc/debug_level
-echo 2 > /sys/kernel/debug/color_page_alloc/enable
+echo -n "colors: "
+cat /sys/kernel/debug/color_page_alloc/colors
+echo 4 > /sys/kernel/debug/color_page_alloc/debug_level
 
