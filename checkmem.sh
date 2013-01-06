46f33e1e6dade98eb2c85639570047fbb692a188
Ubuntu <heechul@schedtest.defaultdomain>
2013-01-06 16:44:14 +0000

    add check memory

---
+++ checkmem.sh
@@ -0,0 +1,22 @@
+msize=$1
+
+[ -z "$msize" ] && exit 1
+
+reset_stat()
+{
+    echo reset > /sys/kernel/debug/color_page_alloc/core
+    echo "reset stat.."
+}
+check_mem()
+{
+    echo "> $1"
+    cat /proc/buddyinfo | tail -n 15
+    cat /sys/kernel/debug/color_page_alloc/core
+}
+reset_stat
+check_mem "Before"
+./bandwidth -m $msize -t 5&
+sleep 1
+check_mem "Middle"
+sleep 5
+check_mem "After"
