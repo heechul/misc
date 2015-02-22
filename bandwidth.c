3f8df51f2ef59171b8afdd31ee3b720248f0ab8c
Heechul Yun <heechul@illinois.edu>
2015-02-22 10:44:54 -0600

    bandwidth cleanup

--- bandwidth.c
+++ bandwidth.c
@@ -13,9 +13,6 @@
 /**************************************************************************
  * Conditional Compilation Options
  **************************************************************************/
-#define P4080_MCTRL_INTRV_NONE 1
-#define P4080_MCTRL_INTRV_CLCS 0
-#define XEON 0
 
 /**************************************************************************
  * Included Files
@@ -38,27 +35,18 @@
 /**************************************************************************
  * Public Definitions
  **************************************************************************/
-#if XEON
-#  define ADDR_2ND_RANK 0x80000000
-#else 
-#  define ADDR_2ND_RANK 0x40000000   /* the address of the 2nd Rank(1GB) of */
-#endif
-
 #define CACHE_LINE_SIZE 64	   /* cache Line size is 64 byte */
-#define DRAM_ROW_SIZE 1024
+#define DEFAULT_ALLOC_SIZE_KB 16384
 
 /**************************************************************************
  * Public Types
  **************************************************************************/
-enum access_type { READ, WRITE, RDWR, WRST};
+enum access_type { READ, WRITE};
 
 /**************************************************************************
  * Global Variables
  **************************************************************************/
-int g_mem_size = 16384 * 1024;	   /* memory size */
-int g_indx = 0;			   /* global index used for accessing the array */
-int g_next = (CACHE_LINE_SIZE/4);  /* incrementing the next element in the array 
-				      (Sequtial Default) */
+int g_mem_size = DEFAULT_ALLOC_SIZE_KB * 1024;	   /* memory size */
 int *g_mem_ptr = 0;		   /* pointer to allocated memory region */
 
 FILE *g_fd = NULL;			
@@ -97,68 +85,11 @@ void quit(int param)
 	exit(0);
 }
 
-/*
-void
-rd(iter_t iterations, void *cookie)
-{
-        state_t *state = (state_t *) cookie;
-        register TYPE *lastone = state->lastone;
-        register int sum = 0;
-
-        while (iterations-- > 0) {
-            register TYPE *p = state->buf;
-            while (p <= lastone) {
-                sum +=
-#define DOIT(i) p[i]+
-                DOIT(0) DOIT(4) DOIT(8) DOIT(12) DOIT(16) DOIT(20) DOIT(24)
-                DOIT(28) DOIT(32) DOIT(36) DOIT(40) DOIT(44) DOIT(48) DOIT(52)
-                DOIT(56) DOIT(60) DOIT(64) DOIT(68) DOIT(72) DOIT(76)
-                DOIT(80) DOIT(84) DOIT(88) DOIT(92) DOIT(96) DOIT(100)
-                DOIT(104) DOIT(108) DOIT(112) DOIT(116) DOIT(120)
-                p[124];
-                p +=  128;
-            }
-        }
-        use_int(sum);
-}
-#undef  DOIT
-*/
-
-int bench_read_lmbench()
-{
-	int i;
-	int sum = 0;    
-	register int *p = (int *)g_mem_ptr;
-	while ( p < (int *)&g_mem_ptr[g_mem_size/4]) {
-		sum += 
-#define DOIT(i) p[i]+
-                DOIT(0) 
-		DOIT(4)
-		DOIT(8)
-DOIT(12) DOIT(16) DOIT(20) DOIT(24)
-                DOIT(28) 
-		DOIT(32) 
-		DOIT(36) DOIT(40) DOIT(44) DOIT(48) DOIT(52)
-                DOIT(56) DOIT(60) 
-		DOIT(64) 
-		DOIT(68) DOIT(72) DOIT(76)
-                DOIT(80) DOIT(84) DOIT(88) DOIT(92) 
-                DOIT(96)
-                DOIT(100) DOIT(104) DOIT(108) DOIT(112) DOIT(116) DOIT(120)
-#if 0
-#endif
-                p[124];
-                p +=  128;
-	}
-	g_nread += g_mem_size;
-	return sum;
-}
-
 int bench_read()
 {
-	int i;	
-	int sum = 0;
-	for ( i = 0; i < g_mem_size/4; i+=16 ) {
+	register int i;	
+	register int sum = 0;
+	for ( i = 0; i < g_mem_size/4; i+=(CACHE_LINE_SIZE/4) ) {
 		sum += g_mem_ptr[i];
 	}
 	g_nread += g_mem_size;
@@ -167,32 +98,19 @@ int bench_read()
 
 int bench_write()
 {
-	int i;	
-	for ( i = g_indx; i < g_mem_size/4; i+=g_next ) {
-		g_nread += CACHE_LINE_SIZE ;
+	register int i;	
+	for ( i = 0; i < g_mem_size/4; i+=(CACHE_LINE_SIZE/4) ) {
 		g_mem_ptr[i] = i;
 	}
+	g_nread += g_mem_size;
 	return 1;
-}
-
-int bench_rdwr()
-{
-	int i;
-	int sum = 0;    
-
-	for ( i = g_indx; i < g_mem_size/4; i+=g_next ) {
-		sum += g_mem_ptr[i];
-		g_mem_ptr[i] = i;
-		g_nread += CACHE_LINE_SIZE;
-	}
-	return sum;
 }
 
 void usage(int argc, char *argv[])
 {
 	printf("Usage: $ %s [<option>]*\n\n", argv[0]);
 	printf("-m: memory size in KB. deafult=8192\n");
-	printf("-a: access type - read, write, rdwr. default=read\n");
+	printf("-a: access type - read, write. default=read\n");
 	printf("-n: addressing pattern - Seq, Row, Bank. default=Seq\n");
 	printf("-t: time to run in sec. 0 means indefinite. default=5. \n");
 	printf("-c: CPU to run.\n");
@@ -200,7 +118,6 @@ void usage(int argc, char *argv[])
 	printf("-p: priority\n");
 	printf("-l: log label. use together with -f\n");
 	printf("-f: log file name\n");
-	printf("-x: memory map to /dev/mem. !!! DANGEROUS !!!\n");
 	printf("-h: help\n");
 	printf("\nExamples: \n$ bandwidth -m 8192 -a read -t 1 -c 2\n  <- 8MB read for 1 second on CPU 2\n");
 	exit(1);
@@ -215,7 +132,6 @@ int main(int argc, char *argv[])
 	int acc_type = READ;
 	int opt;
 	cpu_set_t cmask;
-	int use_mmap = 0;
 	int iterations = 0;
 	int i;
 
@@ -232,27 +148,10 @@ int main(int argc, char *argv[])
 				acc_type = READ;
 			else if (!strcmp(optarg, "write"))
 				acc_type = WRITE;
-			else if (!strcmp(optarg, "rdwr"))
-				acc_type = RDWR;
 			else
 				exit(1);
 			break;
 			
-		case 'n': /* set access pattern */
-			/* sequential */
-			if( strcmp(optarg,"Seq") == 0 ) {
-				g_indx = 0;
-				g_next = (CACHE_LINE_SIZE/4);				
-			}
-			/* same bank */
-			else if( strcmp(optarg,"Row") == 0 ) {
-				g_indx = 0;
-				g_next = DRAM_ROW_SIZE / 4;
-			}
-			else
-				exit(1);
-			break;
-
 		case 't': /* set time in secs to run */
 			finish = strtol(optarg, NULL, 0);
 			break;
@@ -287,60 +186,26 @@ int main(int argc, char *argv[])
 			if (g_fd == NULL) 
 				perror("error");
 			break;
-		case 'x': /* mapping to /dev/mem. !! DANGEROUS !! */
-			use_mmap = 1;
-			break;
 		case 'h': 
 			usage(argc, argv);
 			break;
 		}
 	}
 
-	g_indx *= cpuid;
-
 	/*
 	 * allocate contiguous region of memory 
 	 */ 
-	if (use_mmap) {
-		/* open /dev/mem for accessing memory in physical addr. */
-		int fd = -1;
-		unsigned long offset;
-
-		fprintf(stderr, "Use mmap| g_indx: 0x%x g_next: 0x%x\n", g_indx, g_next);
-		fd = open("/dev/mem", O_RDWR | O_SYNC);
-		if(fd == -1) {
-			fprintf(stderr, "ERROR Opening /dev/mem\n");	
-			exit(1);
-		} 
-		/* offset variable is used to allocate each cpu to a different offset 
-		   from each other */
-		offset = ADDR_2ND_RANK; /*  + cpuid*g_mem_size;*/
-		fprintf(stderr, "offset: %p\n", (void *)offset);
-		/* use mmap to allocate each cpu to the specific address in memory */
-		g_mem_ptr = (int *)mmap(NULL, g_mem_size, PROT_READ|PROT_WRITE, 
-					MAP_SHARED, fd, offset);
-		if(g_mem_ptr == NULL) {
-			fprintf(stderr, "could not allocate memarea");
-			exit(1);
-		}
-		fprintf(stderr, "mmap was successful: addr=%p\n", g_mem_ptr);
-	} else {
-		printf("Use standard malloc\n");
-		g_mem_ptr = (int *)malloc(g_mem_size);
-	}
+	g_mem_ptr = (int *)malloc(g_mem_size);
 
 	for (i = 0; i < g_mem_size / sizeof(int); i++)
 		g_mem_ptr[i] = i;
 
 	memset((char *)g_mem_ptr, 1, g_mem_size);
-	fprintf(stderr, "VADDR: %p-%p\n", (char *)g_mem_ptr, (char *)g_mem_ptr + g_mem_size);
 
 	/* print experiment info before starting */
 	printf("memsize=%d KB, type=%s, cpuid=%d\n",
 	       g_mem_size/1024,
-	       ((acc_type==READ) ?"read":
-		(acc_type==WRITE)? "write" :
-		(acc_type==RDWR) ? "rdwr" : "worst"),
+	       ((acc_type==READ) ?"read": "write"),
 		cpuid);
 	printf("stop at %d\n", finish);
 
@@ -362,9 +227,6 @@ int main(int argc, char *argv[])
 			break;
 		case WRITE:
 			sum += bench_write();
-			break;
-		case RDWR:
-			sum += bench_rdwr();
 			break;
 		}
 
